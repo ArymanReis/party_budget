b0e676da5ce3671b68b1549c98ab173c01d73fc3
Aryman Reis <arymanreis@gmail.com>
2024-06-28 21:03:33 -0300

    Avançando

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -22,6 +22,55 @@ const serviceController = {
             console.log(error);
         }
 
+    },
+
+    getAll: async (req, res) => {
+        try {
+            const services = await ServiceModel.find();
+
+            res.json(services);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    get: async(req, res) => {
+        try {
+            
+            const id = req.params.id
+            const service = await ServiceModel.findById(id);
+
+            if(!service) {
+                res.status(404).json({ msg: "Serviços não encontrado."});
+                return;
+            }
+
+            res.json(service);
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    delete: async(req, res) => {
+        try {
+            
+            const id = req.params.id; 
+
+            const service = await ServiceModel.findById(id);
+
+            if(!service) {
+                res.status(404).json({ msg: "Serviços não encontrado."});
+                return;
+            }
+
+            const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+            res
+            .status(200)
+            .json({deletedService, msg: "Serviço excluído com sucesso"});
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
 };
